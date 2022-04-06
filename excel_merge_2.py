a13fc637d8623037c95524bf0738db3aff4be548
hongmsz <hongmsz@gmail.com>
2022-04-06 13:57:33 +0900

    Add files via upload

---
+++ excel_merge_2.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import string
+import os.path
+
+f_pos = 'D:/CSV/ex/'
+f_type = '.xlsx'
+f_list_get = pd.read_excel(f_pos + 'KB계좌확인' + f_type, header=None)
+#f_list = f_list_get[0].values.tolist()
+f_list = f_list_get[4].values.tolist()
+del f_list[0]
+del f_list[-2:]
+
+acc_name=[]
+for a in f_list:
+    acc_name.append(a[9:])
+
+tmp = pd.read_excel(f_pos + f_list[0] + f_type)
+e_tmp1 = tmp.iloc[1::2,:]
+e_tmp1.reset_index(drop=True , inplace=True)
+e_tmp2 = tmp.iloc[0::2,:]
+e_tmp2 = e_tmp2.rename(columns=e_tmp2.iloc[0])
+e_tmp2 = e_tmp2.drop(e_tmp2.columns[0], axis=1)
+e_tmp2 = e_tmp2.drop(e_tmp2.index[0])
+e_tmp2.reset_index(drop=True , inplace=True)
+result2 = pd.concat([e_tmp1,e_tmp2], axis=1)
+
+result2['ACC_NUM'] = acc_name[0]
+
+for i in range(1, len(f_list)):
+    if os.path.isfile(f_pos + f_list[i] + f_type):
+        tmp = pd.read_excel(f_pos + f_list[i] + f_type)
+        e_tmp1 = tmp.iloc[1::2,:]
+        e_tmp1.reset_index(drop=True , inplace=True)
+        e_tmp2 = tmp.iloc[0::2,:]
+        e_tmp2 = e_tmp2.rename(columns=e_tmp2.iloc[0])
+        e_tmp2 = e_tmp2.drop(e_tmp2.columns[0], axis=1)
+        e_tmp2 = e_tmp2.drop(e_tmp2.index[0])
+        e_tmp2.reset_index(drop=True , inplace=True)
+        result1 = pd.concat([e_tmp1,e_tmp2], axis=1)
+        
+        result1['ACC_NUM'] = acc_name[i]
+        
+        result2 = pd.concat([result2,result1], ignore_index=True)
+
+result2.to_excel(f_pos + 'merge_excel_202201' + f_type)
