1bdef93898dac68dda86ce49eeda69deb6ae6b90
hongmsz <hongmsz@gmail.com>
2022-11-18 11:16:52 +0900

    Update excel_merge_2.py

--- excel_merge_2.py
+++ excel_merge_2.py
@@ -31,18 +31,19 @@ result2 = pd.concat([e_tmp1,e_tmp2], axis=1)
 result2['ACC_NUM'] = acc_name[0]
 
 for i in range(1, len(f_list)):
-    tmp = pd.read_excel(f_pos + f_list[i] + f_type)
-    e_tmp1 = tmp.iloc[1::2,:]
-    e_tmp1.reset_index(drop=True , inplace=True)
-    e_tmp2 = tmp.iloc[0::2,:]
-    e_tmp2 = e_tmp2.rename(columns=e_tmp2.iloc[0])
-    e_tmp2 = e_tmp2.drop(e_tmp2.columns[0], axis=1)
-    e_tmp2 = e_tmp2.drop(e_tmp2.index[0])
-    e_tmp2.reset_index(drop=True , inplace=True)
-    result1 = pd.concat([e_tmp1,e_tmp2], axis=1)
-    
-    result1['ACC_NUM'] = acc_name[i]
-    
-    result2 = pd.concat([result2,result1], ignore_index=True)
+    if os.path.isfile(f_pos + f_list[i] + f_type):
+        tmp = pd.read_excel(f_pos + f_list[i] + f_type)
+        e_tmp1 = tmp.iloc[1::2,:]
+        e_tmp1.reset_index(drop=True , inplace=True)
+        e_tmp2 = tmp.iloc[0::2,:]
+        e_tmp2 = e_tmp2.rename(columns=e_tmp2.iloc[0])
+        e_tmp2 = e_tmp2.drop(e_tmp2.columns[0], axis=1)
+        e_tmp2 = e_tmp2.drop(e_tmp2.index[0])
+        e_tmp2.reset_index(drop=True , inplace=True)
+        result1 = pd.concat([e_tmp1,e_tmp2], axis=1)
+
+        result1['ACC_NUM'] = acc_name[i]
+
+        result2 = pd.concat([result2,result1], ignore_index=True)
 
 result2.to_excel(f_pos + 'merge_excel' + f_type)
